6ea8f2bc4e528075775490aab45cce2f3891b604
Your Name <you@example.com>
2025-02-16 16:54:07 +0100

    Add program

--- wyzwanie nr3/Program.cs
+++ wyzwanie nr3/Program.cs
@@ -1,10 +1,37 @@
-﻿namespace wyzwanie_nr3
+﻿
+namespace wyzwanie_nr3
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.WriteLine("Wprowadz liczbę");
+            var liczba = GetNumber();
+            
+        }
+        private static int GetNumber()
+        {
+            while (true)
+            {
+                if (!int.TryParse(Console.ReadLine(),  out int a))
+                {
+                    Console.WriteLine("Wprowadź liczbę!!");
+                    continue;
+                }
+                if ((a % 2) == 0 && a!=0)
+                {
+                    Console.WriteLine("wpisałeś liczbę parzystą");
+                }
+                else if ((a % 2) == 1)
+                {
+                    Console.WriteLine("wpisałeś liczbę nieparzystą");
+                }
+                else if (a==0)
+                {
+                    Console.WriteLine("Wprowadziłeś zero");
+                }
+                //return a;
+            }
         }
     }
 }
